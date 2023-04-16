1edf05d974159b4219afaa9b2ed0df1b4ae8e352
Intel <minecraftjava999@gmail.com>
2023-04-16 17:10:06 +0100

    Update Log.java

--- src/main/java/me/intel/os/utils/Log.java
+++ src/main/java/me/intel/os/utils/Log.java
@@ -6,16 +6,16 @@ public class Log {
     public void info(Object o) {
         LocalTime time = LocalTime.now();
         System.out.println(String.format("[%02d:%02d:%02d]: [%s]: %s",
-                time.getHour(), time.getMinute(), time.getSecond(), "Test", message));
+                time.getHour(), time.getMinute(), time.getSecond(), "INFO", o.toString()));
     }
     public void warn(Object o) {
         LocalTime time = LocalTime.now();
         System.out.println(String.format("[%02d:%02d:%02d]: [%s]: %s",
-                time.getHour(), time.getMinute(), time.getSecond(), "Test", message));
+                time.getHour(), time.getMinute(), time.getSecond(), "WARN", o.toString()));
     }
     public void error(Object o) {
         LocalTime time = LocalTime.now();
         System.out.println(String.format("[%02d:%02d:%02d]: [%s]: %s",
-                time.getHour(), time.getMinute(), time.getSecond(), "Test", message));
+                time.getHour(), time.getMinute(), time.getSecond(), "ERROR", o.toString()));
     }
 }
