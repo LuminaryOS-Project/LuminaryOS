b2ab37a964cd0d65e6bed26f17d12eab477c2e3a
Intel <minecraftjava999@gmail.com>
2023-04-17 18:01:01 +0100

    Create PWDCommand.java

---
+++ src/main/java/me/intel/os/commands/impl/PWDCommand.java
@@ -0,0 +1,23 @@
+package me.intel.os.commands.impl;
+
+import me.intel.os.OS;
+import me.intel.os.commands.Command;
+import me.intel.os.permissions.PermissionLevel;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+public class PWDCommand extends Command {
+   public PWDCommand() {
+      super("pwd", "pwd / cwd", Arrays.asList("cwd", "pwd"));
+   }
+
+   public void execute(List<String> args) {
+      System.out.println("Current Directory: " + OS.currentDir);
+   }
+
+   public PermissionLevel getPremission() {
+      return PermissionLevel.USER;
+   }
+}
